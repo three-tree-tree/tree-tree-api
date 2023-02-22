ed5f4d1d684d6d313dbca3be3414cf6b13e52106
ssunday0611 <colorpicker06@gmail.com>
2023-02-23 00:28:03 +0900

    guild gradle 충돌 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
-	kotlin("plugin.allopen") version "1.7.22"
-	kotlin("plugin.noarg") version "1.7.22"
+    kotlin("plugin.allopen") version "1.7.22"
+    kotlin("plugin.noarg") version "1.7.22"
 }
 
 group = "com.treetree"
@@ -20,21 +20,13 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("mysql:mysql-connector-java:8.0.32")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.modelmapper:modelmapper:2.1.1")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("mysql:mysql-connector-java:8.0.32")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.modelmapper:modelmapper:2.1.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -46,12 +38,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-allOpen {
-	annotation("javax.persistence.Entity")
-}
-
-noArg {
-	annotation("javax.persistence.Entity")
 }
